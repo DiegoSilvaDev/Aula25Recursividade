3ed3c1f9743bd9cce2f7e30d04087760cfaa34c5
DiegoSilvaDev <Diego2102.ds@gmail.com>
2020-06-25 15:19:35 -0300

    Ok

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade rec = new Recursividade();
+            rec.GerarSequenciaFibonacci(0,1,15);
+            System.Console.WriteLine();
+            Console.WriteLine(rec.GerarFatorial(5));
+            rec.GerarSequenciaTribonacci(1,1,2,15);
+
+        }
+    }
+}
---
+++ Recursividade.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Recursividade
+{
+    public class Recursividade
+    {
+        
+
+        // GerarSequenciaFibonacci(0, 1, 15)
+        // GerarSequenciaFibonacci(1, 1, 14)
+        // GerarSequenciaFibonacci(1, 2, 13)
+        // GerarSequenciaFibonacci(2, 3, 12)
+        // GerarSequenciaFibonacci(3, 5, 11)
+        public void GerarSequenciaFibonacci(int num1, int num2, int vezes){
+            
+            // Condição crucial da parada da recursividade
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            if(vezes > 0){
+                Console.WriteLine(num1);
+                GerarSequenciaFibonacci(num2, num1 + num2, vezes - 1);
+            }
+            Console.ResetColor();
+        }
+
+        public void GerarSequenciaTribonacci(int num1, int num2, int num3, int vezes){
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if(vezes >= 0){
+                Console.WriteLine(num1);
+                GerarSequenciaTribonacci(num2 ,num3, num1 + num2 + num3, vezes -1);
+            }
+            Console.ResetColor();
+        }
+
+
+        // 5! = 5 x 4 x 3 x 2 x 1 = 120
+        public int GerarFatorial(int numero){
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            if(numero == 1){
+                return 1;
+            }else{
+                return numero * GerarFatorial(numero - 1);
+            }
+        }
+
+        
+    }
+}
